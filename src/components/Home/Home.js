951bab2640e4249abfacd645435c41401a069561
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-04 01:28:12 +0600

    design home page header section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home Component</h1>
+  return (
+    <div className="container mt-5">
+      <section>
+        <div className="row">
+          <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+            <img
+              src="https://images.deliveryhero.io/image/fd-bd/LH/h6f4-listing.jpg?width=400&height=292"
+              alt="food_image"
+              className="img-fluid rounded-3 w-100"
+            />
+          </div>
+          <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
+            <div className="w-75">
+              <h1>Hello-Foods</h1>
+              <p>
+                This is a food delivery website. This is a food delivery
+                website. This is a food delivery website. This is a food
+                delivery website.
+              </p>
+              <button className="btn btn-info">Learn More</button>
+            </div>
+          </div>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Home;
