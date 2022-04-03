a1a43ac8d93d6e94cccff629f82c1662ea6b6c77
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-04 01:11:23 +0600

    Update not found page design

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className="container mt-5">
+      <div class="card bg-dark p-3">
+        <div class="card-body text-info display-6">Page Not Found</div>
+      </div>
     </div>
   );
 };
