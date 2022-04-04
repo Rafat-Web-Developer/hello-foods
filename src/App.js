dc24c26c06008dbe899e3dba3adc0dd7a20ddfaf
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-04-04 11:49:01 +0600

    Add line and Bar chart in dashboard component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Reviews from "./components/Reviews/Reviews";
 import NotFound from "./components/NotFound/NotFound";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
